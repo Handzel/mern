7ffb696c9e3eb0c4009250f6531cbca982219f7c
Handzel <handzelewicz.dev@gmail.com>
2018-07-10 15:49:44 +0200

    added /api/items route + POST & GET methods

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser middleware
 app.use(bodyParser.json());
 
+
 // DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to mongo
+// Connect to Mongo
 mongoose.connect(db)
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
+
+// items route
+app.use('/api/items', items);
 
 const port = process.env.PORt || 5005;
 
